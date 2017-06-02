8dcb2b535114d2da59aa332e5a2cb90e205f3b92
gavbn <wjw69763@xzsok.com>
2017-06-02 18:53:09 +0530

    Update server.go

--- server/server.go
+++ server/server.go
@@ -14,12 +14,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gavbn/tbn/engine"
-	"github.com/gavbn/tbn/static"
+	"github.com/jpillora/cloud-torrent/engine"
+	"github.com/jpillora/cloud-torrent/static"
 	"github.com/jpillora/go-realtime"
 	"github.com/jpillora/requestlog"
 	"github.com/jpillora/scraper/scraper"
-	"github.com/skratchdot/open-golang/open"
+        "github.com/skratchdot/open-golang/open"
 )
 
 //Server is the "State" portion of the diagram
